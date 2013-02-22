9503d500937df366ba9c8295c569f8f29d53ac7e
aeischeid <aeischeid@aeischeid-acer.(none)>
2013-02-21 20:51:45 -0600

    a few more tests, I expect this would be a tricky one

--- test/specs/controller.js
+++ test/specs/controller.js
@@ -90,4 +90,69 @@ describe("Controller", function(){
     var users = new Users();
     expect(users.el.attr("style")).toEqual("width: 100%");
   });
+  
+  it("can listen to events on a model instance", function(){
+    var Asset = Spine.Model.setup("Asset", ["name"]);
+    var asset = Asset.create({name: "test.pdf"});
+    var users = new Users();
+    var noop = {spy: function(){}};
+    spyOn(noop, "spy");
+    spy = noop.spy;
+    users.listenTo(asset, 'event1 event2', spy);
+    asset.trigger("event1");
+    expect(spy).toHaveBeenCalled();
+  });
+  
+  it("can listen once for an event on a model instance", function(){
+    var Asset = Spine.Model.setup("Asset", ["name"]);
+    var asset = Asset.create({name: "test.pdf"});
+    var users = new Users();
+    var noop = {spy: function(){}};
+    spyOn(noop, "spy");
+    spy = noop.spy;
+    users.listenToOnce(asset, 'event1', spy);
+    asset.trigger("event1");
+    expect(spy).toHaveBeenCalled();
+    spy.reset();
+    asset.trigger("event1");
+    expect(spy).not.toHaveBeenCalled();
+  });
+  
+  it("can stop listening to events on a model instance", function(){
+    var Asset = Spine.Model.setup("Asset", ["name"]);
+    var asset = Asset.create({name: "test.pdf"});
+    var users = new Users();
+    var noop = {spy: function(){}};
+    spyOn(noop, "spy");
+    spy = noop.spy;
+    users.listenTo(asset, 'event1', spy);
+    asset.trigger("event1");
+    expect(spy).toHaveBeenCalled();
+    spy.reset();
+    users.stopListening(asset, 'event1', spy);
+    asset.trigger("event1");
+    expect(spy).not.toHaveBeenCalled();
+  });
+  
+  it("can stop listening to events on a model instance, without canceling out other binders on that model instance", function(){
+    var Asset = Spine.Model.setup("Asset", ["name"]);
+    var asset = Asset.create({name: "test.pdf"});
+    var users = new Users();
+    var noop = {spy: function(){}, spy2: function(){}};
+    spyOn(noop, "spy");
+    spyOn(noop, "spy2");
+    spy = noop.spy;
+    spy2 = noop.spy2;
+    Asset.bind('event1', spy2)
+    users.listenTo(asset, 'event1', spy);
+    asset.trigger("event1");
+    expect(spy).toHaveBeenCalled();
+    expect(spy2).toHaveBeenCalled();
+    spy.reset();
+    spy2.reset();
+    users.stopListening(asset, 'event1');
+    asset.trigger("event1");
+    expect(spy).not.toHaveBeenCalled();
+    expect(spy2).toHaveBeenCalled();
+  });
 });
